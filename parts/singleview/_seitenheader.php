ead7f4d8b525a95ad6821729b24791b3089992bc
Killingova <k.zhivkova@bbqberlin.onmicrosoft.com>
2024-02-15 12:24:38 +0100

    Viel Spass

--- parts/singleview/_seitenheader.php
+++ parts/singleview/_seitenheader.php
@@ -12,7 +12,6 @@ Wosny.net – Ihr Spezialist für Bücher und mehr!
                 <a class="nav-link text-warning" href="#" data-toggle="modal" data-target="#loginModal">Anmelden</a>
             </div>
         </div>
-    </div>
     <div id="loginModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -23,7 +22,8 @@ Wosny.net – Ihr Spezialist für Bücher und mehr!
                     </button>
                 </div>
                 <div class="modal-body bg-dark text-white">
-                    <form>
+                    <!-- Beginn des Formular-Tags mit action-Attribut, das auf login.php zeigt -->
+                    <form action="login.php" method="post">
                         <div class="mb-3">
                             <input type="radio" id="customerType" name="userType" value="customer" checked>
                             <label for="customerType" class="mr-2">Kunde</label>
@@ -32,14 +32,15 @@ Wosny.net – Ihr Spezialist für Bücher und mehr!
                         </div>
                         <div class="form-group">
                             <label for="emailInput">E-Mail</label>
-                            <input type="email" class="form-control" id="emailInput" placeholder="E-Mail eingeben">
+                            <input type="email" class="form-control" id="emailInput" name="emailInput" placeholder="E-Mail eingeben">
                         </div>
                         <div class="form-group">
                             <label for="passwordInput">Passwort</label>
-                            <input type="password" class="form-control" id="passwordInput" placeholder="Passwort">
+                            <input type="password" class="form-control" id="passwordInput" name="passwordInput" placeholder="Passwort">
                         </div>
                         <button type="submit" class="btn btn-warning btn-block">Anmelden</button>
                     </form>
+                    <!-- Ende des Formular-Tags -->
                 </div>
                 <div class="modal-footer bg-dark">
                     <span class="text-white">Ich möchte ein Konto anlegen</span>
@@ -48,4 +49,5 @@ Wosny.net – Ihr Spezialist für Bücher und mehr!
             </div>
         </div>
     </div>
+
 </nav>
